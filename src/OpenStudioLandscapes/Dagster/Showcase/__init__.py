e78d32cc5009e261c213bd57c0f00e2eb43fc781
Michael Mussato <michimussato@gmail.com>
2026-01-24 14:17:25 +0100

    - change namespaces for dist

--- src/OpenStudioLandscapes/Dagster/Showcase/__init__.py
+++ src/OpenStudioLandscapes/Dagster/Showcase/__init__.py
@@ -1,3 +1,4 @@
+import pathlib
 import sys
 from importlib import metadata
 from pathlib import Path
@@ -16,13 +17,15 @@ else:
     raise RuntimeError("Python version >= 3.11 required.")
 
 try:
-    # Change here if project is renamed and does not equal the package name
-    namespace1: str = Path(__file__).parent.parent.parent.name
-    namespace2: str = Path(__file__).parent.parent.name
-    # OpenStudioLandscapes
-    package: str = Path(__file__).parent.name
-    # NukeRLM_8
-    dist: Distribution = metadata.distribution(".".join((namespace1, namespace2, package)))
+    namespace: Path = Path(__file__).parent
+    package = namespace.name
+
+    namespaces = [package]
+
+    while not namespace.name == "OpenStudioLandscapes":
+        namespaces.insert(0, namespace.name)
+        namespace = namespace.parent
+    dist: Distribution = metadata.distribution(".".join(namespaces))
 
     __version__: str = version(dist.name)
 except PackageNotFoundError:  # pragma: no cover
