70a9d60cbad614cae8351714eb681a1b8d032912
Michael Mussato <michimussato@gmail.com>
2026-01-24 14:09:59 +0100

    - change namespaces for dist

--- src/OpenStudioLandscapes/Dagster/Showcase/__init__.py
+++ src/OpenStudioLandscapes/Dagster/Showcase/__init__.py
@@ -1,6 +1,9 @@
 import sys
 from importlib import metadata
 from pathlib import Path
+from dagster import get_dagster_logger
+
+LOGGER = get_dagster_logger(__name__)
 
 if sys.version_info[:2] >= (3, 11):
     # TODO: Import directly (no need for conditional) when `python_requires = >= 3.8`
@@ -14,14 +17,16 @@ else:
 
 try:
     # Change here if project is renamed and does not equal the package name
-    namespace: str = Path(__file__).parent.parent.name
+    namespace1: str = Path(__file__).parent.parent.parent.name
+    namespace2: str = Path(__file__).parent.parent.name
     # OpenStudioLandscapes
     package: str = Path(__file__).parent.name
     # NukeRLM_8
-    dist: Distribution = metadata.distribution(".".join((namespace, package)))
+    dist: Distribution = metadata.distribution(".".join((namespace1, namespace2, package)))
 
     __version__: str = version(dist.name)
 except PackageNotFoundError:  # pragma: no cover
+    LOGGER.error("Can't find metadata.distribution based on namespaces")
     __version__: str = "unknown"
 finally:
     del version, PackageNotFoundError
